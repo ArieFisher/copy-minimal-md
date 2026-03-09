17babced69235f252f437b2f1637cd3fda45b0b1
ArieFisher <ariefisher1@gmail.com>
2026-03-09 12:58:41 -0400

    fix(background): verify URL begins with http://, https://, or file:// before injecting content script (#5)

--- background.js
+++ background.js
@@ -1,10 +1,15 @@
 chrome.commands.onCommand.addListener((command) => {
     if (command === "run-markdown-clean") {
         chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-            chrome.scripting.executeScript({
-                target: { tabId: tabs[0].id },
-                files: ["turndown.js", "turndown-plugin-gfm.js", "content.js"]
-            });
+            const currentTab = tabs[0];
+            if (currentTab && currentTab.url && (currentTab.url.startsWith("http://") || currentTab.url.startsWith("https://") || currentTab.url.startsWith("file://"))) {
+                chrome.scripting.executeScript({
+                    target: { tabId: currentTab.id },
+                    files: ["turndown.js", "turndown-plugin-gfm.js", "content.js"]
+                });
+            } else {
+                console.log("Docs Markdown Cleaner: URL is not supported for text extraction.");
+            }
         });
     }
 });
